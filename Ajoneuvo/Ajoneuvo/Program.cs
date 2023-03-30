292e6b24191fcda02af4f4c589e97587068cc54f
ForgottenTraveler <Antti.Nguyen@Student.Careeria.fi>
2023-03-30 13:39:43 +0300

    Projekti Commit

---
+++ Ajoneuvo/Ajoneuvo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ajoneuvo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        koira koiraOlio = new koira("Rekku", "Saksanpaimenkoira", 4, "Harmaa", 32.0f);
+
+        koiraOlio.nimi = "Rekku";
+        koiraOlio.Rotu = "Saksanpaimenkoira";
+        koiraOlio.Ikä = 4;
+        koiraOlio.väri = "Harmaa";
+        koiraOlio.Paino = 32.0f;
+
+        koiraOlio.Hauku();
+
+        Console.WriteLine(koiraOlio.HaeTiedot());
+
+        koiraOlio.Hauku();
+    }
+}
---
+++ Ajoneuvo/Ajoneuvo/koira.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajoneuvo
+{
+    class koira
+    {
+        public string nimi { get; set; }
+        public string Rotu { get; set;}
+        public int Ikä { get; set; }
+        public String väri { get; set; }
+        public float Paino { get; set; }
+
+        public koira(string _nimi, string _rotu, int _ikä, string _väri, float _paino) 
+        { 
+            nimi = _nimi;
+            Rotu = _rotu;
+            Ikä = _ikä;
+            väri = _väri;
+            Paino = _paino;
+        }
+        public string HaeTiedot()
+        {
+            string KoiranTiedot = "Nimi: " + nimi + ". Rotu: " + Rotu + ". Ikä: " + Ikä.ToString() + ". Väri: " + väri + ". Paino: " + Paino.ToString();
+            return KoiranTiedot;
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine("Woof woof");
+        }
+    }
+}
