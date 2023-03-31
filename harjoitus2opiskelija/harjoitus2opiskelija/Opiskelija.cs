77a19574b86e793e1e5d0ab6b372cbca5421fa63
ForgottenTraveler <Antti.Nguyen@Student.Careeria.fi>
2023-03-31 13:19:42 +0300

    Harjoitus2Opiskelija

---
+++ harjoitus2opiskelija/harjoitus2opiskelija/Opiskelija.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus2opiskelija
+{
+    internal class Opiskelija
+    {
+        public string Nimi { get; set; }
+        public string OpiskelijaID { get; set; }
+        public int Opintopisteet { get; set; }
+
+        public Opiskelija(string _nimi, string _OpiskelijaID, int _Opintopisteet)
+        {
+            
+            Nimi = _nimi;
+            OpiskelijaID = _OpiskelijaID;
+            Opintopisteet= _Opintopisteet;
+        }
+        
+        public void Datatulostin()
+        {
+            string opiskelijatiedot = "nimi: " + Nimi + ". OpiskelijaID: " + OpiskelijaID + ". Opintopisteet: " ;
+            Console.Write(opiskelijatiedot);
+        }
+        public void Opintopisteenmuokkaaja() 
+        {
+            Random RandPiste = new Random();
+            Opintopisteet = RandPiste.Next(5, 25);
+            Console.WriteLine(Opintopisteet);
+        }   
+    }
+}
---
+++ harjoitus2opiskelija/harjoitus2opiskelija/Program.cs
@@ -0,0 +1,32 @@
+﻿using harjoitus2opiskelija;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 0;
+
+        while (true)
+        {
+            Random Genname = new Random();
+            int Gennamae = Genname.Next(0, 20);
+            string[] genename = { "Rekku", "Rakku", "Rokku", "Rekka", "Riukka", "Roani", "Rellas", "Rollas", "Renpa", "Ronpa", "Joule", "juol", "Joum", "Bana", "Bon", "Buyn", "Jan", "Jen", "june", "Roiski", "Ruilis" };
+
+            Random Idgen = new Random();
+            int Idgener = Idgen.Next(1, 50);
+
+            Opiskelija OpiskeliaOlio = new Opiskelija(genename[Gennamae], Idgener.ToString(), 25);
+
+            OpiskeliaOlio.Datatulostin();
+            OpiskeliaOlio.Opintopisteenmuokkaaja();
+            
+            i++;
+            if (i >= 5)
+            {
+                break; 
+            }
+               
+        }
+        
+    }
+}
