58cb84827cd571120d477f3e06b96aab84238197
ForgottenTraveler <Antti.Nguyen@Student.Careeria.fi>
2023-04-06 12:54:54 +0300

    Harjoitus4Albumi(Kt)

--- Albumi(KT)/Albumi(KT)/Program.cs
+++ Albumi(KT)/Albumi(KT)/Program.cs
@@ -5,14 +5,14 @@ internal class Program
     private static void Main(string[] args)
     {
         Kappale[] kappaleet = {
-                new Kappale("---Snail's House - bloom", "2:40"),
-                new Kappale("---Snail's House - if (ujbeats vol.1)", "3:53"),
-                new Kappale("---Snail's House - amayadori", "3:14"),
-                new Kappale("---Snail's House - d'amour (ujbeat mix)", "2:29"),
-                new Kappale("---Snail's House - ramune (ujbeats mix)", "3:18"),
-                new Kappale("---Snail's House - sweetheart", "2:02"),
-                new Kappale("---Snail's House - Thinkin' of u.", "2:27"),
-                new Kappale("---Snail's House - dear", "3:23")
+                new Kappale("--- Nimi: Snail's House - bloom", "2:40"),
+                new Kappale("--- Nimi: Snail's House - if (ujbeats vol.1)", "3:53"),
+                new Kappale("--- Nimi: Snail's House - amayadori", "3:14"),
+                new Kappale("--- Nimi: Snail's House - d'amour (ujbeat mix)", "2:29"),
+                new Kappale("--- Nimi: Snail's House - ramune (ujbeats mix)", "3:18"),
+                new Kappale("--- Nimi: Snail's House - sweetheart", "2:02"),
+                new Kappale("--- Nimi: Snail's House - Thinkin' of u.", "2:27"),
+                new Kappale("--- Nimi: Snail's House - dear", "3:23")
             };
 
         Albumi albumi = new Albumi("Ujico*/Snail's House", "ujbeats vol.1" ,"JPop", 25, kappaleet);
