360b82bf895d7d3c2ea29a565991f14344c839d7
ForgottenTraveler <Antti.Nguyen@Student.Careeria.fi>
2023-04-06 10:46:04 +0300

    Harjoitus3kiuas(kt)done

--- harjoitus3kiuas(kt)/harjoitus3kiuas(kt)/Kiuas.cs
+++ harjoitus3kiuas(kt)/harjoitus3kiuas(kt)/Kiuas.cs
@@ -8,5 +8,36 @@ namespace harjoitus3kiuas_kt_
 {
     internal class Kiuas
     {
+        public string Nimi { get; set; }
+        public int Lampotila { get; set; }
+        public int Kosteus { get; set; }
+        public bool Tila { get; set; }
+
+        public Kiuas(string kiuas, int lampotila, int kosteus, bool tila)
+        {
+            Nimi = kiuas;
+            Lampotila = lampotila;
+            Kosteus = kosteus;
+            Tila = tila;
+        }
+        public void TulostaTiedot()
+        {
+            Console.WriteLine("Kiuas: " + Nimi);
+            Console.WriteLine("Lampotila: " + Lampotila);
+            Console.WriteLine("Kosteus: " + Kosteus);
+            Console.WriteLine("Tila: " + (Tila ? "päällä" : "pois päältä"));
+        }
+        public void VaihdaLampotila(int uusiLampotila)
+        {
+            Lampotila = uusiLampotila;
+            Console.WriteLine("Uusi lämpötila asetettu: " + uusiLampotila);
+        }
+
+
+        public void VaihdaKosteus(int uusiKosteus)
+        {
+            Kosteus = uusiKosteus;
+            Console.WriteLine("Uusi kosteus asetettu: " + uusiKosteus);
+        }
     }
 }
--- harjoitus3kiuas(kt)/harjoitus3kiuas(kt)/Program.cs
+++ harjoitus3kiuas(kt)/harjoitus3kiuas(kt)/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using harjoitus3kiuas_kt_;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Kiuas kiuas = new Kiuas("Testikiuas", 0, 0, false);
+        kiuas.TulostaTiedot();
+
+        kiuas.VaihdaLampotila(70);
+        kiuas.VaihdaKosteus(50);
+        kiuas.TulostaTiedot();
+
+        kiuas.Tila = true;
+        kiuas.TulostaTiedot();
+
+        Console.ReadKey();
     }
 }
