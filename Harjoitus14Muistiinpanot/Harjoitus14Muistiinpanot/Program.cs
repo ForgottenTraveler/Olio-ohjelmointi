61875a4198f18627bf17261e79f041e54e770d3c
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-05-04 14:32:15 +0300

    Harjoitus 14 fix

--- Harjoitus14Muistiinpanot/Harjoitus14Muistiinpanot/Program.cs
+++ Harjoitus14Muistiinpanot/Harjoitus14Muistiinpanot/Program.cs
@@ -2,6 +2,32 @@
 {
     private static void Main(string[] args)
     {
+        string lukija;
+        string filepath = @"d:\harjoitus14.txt";
+
+        try
+        {
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine("Muistiinpanot: ");
+                }
+            }
+            bool poistutaanko = false;
+            while (!poistutaanko)
+            {
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Anna kometo: Lisää, Näytä, Poista, Poistu");
+                lukija = Console.ReadLine().ToLower();
+                switch (lukija)
+                {
+                    case "lisää";
+                        LisääMuistiinpano(filepath);
+                        break;
+                }
+            }
+        }
 
     }
 }
