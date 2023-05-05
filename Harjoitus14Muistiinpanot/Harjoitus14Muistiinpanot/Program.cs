236a8ebbbee3286f011b66e9b588845aaf2f2f93
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-05-05 09:36:51 +0300

    Fix commit

--- Harjoitus14Muistiinpanot/Harjoitus14Muistiinpanot/Program.cs
+++ Harjoitus14Muistiinpanot/Harjoitus14Muistiinpanot/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         string lukija;
-        string filepath = @"d:\harjoitus14.txt";
+        string filepath = @"harjoitus14.txt";
 
         try
         {
@@ -22,11 +22,51 @@
                 lukija = Console.ReadLine().ToLower();
                 switch (lukija)
                 {
-                    case "lisää";
+                    case "lisää":
                         LisääMuistiinpano(filepath);
                         break;
+                    case "näytä":
+                        NäytäMuistiinpanot(filepath); 
+                        break;
+                    case "poista":
+                        PoistaMuistiinpanot(filepath); 
+                        break;
+                    case "poistu":
+                        poistutaanko =true;
+                        return;
+                    default:
+                        Console.WriteLine("Annettu komento on virheelinen... anna uusi komento.");
+                        break;
+
                 }
             }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        static void LisääMuistiinpano(string filepath)
+        {
+            string lukija = "";
+            Console.WriteLine("Kirjoita uusi muistiinpano: \n");
+            lukija = Console.ReadLine();
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine(lukija);
+            }
+        }
+        static void NäytäMuistiinpanot(string filepath)
+        {
+            Console.WriteLine();
+            string[] rivit = File.ReadAllLines(filepath);
+            foreach (string rivi in rivit)
+            {
+                Console.WriteLine(rivi);
+            }
+        }
+        static void PoistaMuistiinpanot(string filepath)
+        {
+            File.WriteAllText(filepath, "Muistiinpanot: ");
         }
 
     }
