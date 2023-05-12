f7a519a4f3f2887d7e87fd7048e26006433d6df4
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-05-12 10:10:50 +0300

    Commit fix

--- Harjoitus17TaloWPF(kt)/Harjoitus17TaloWPF(kt)/MainWindow.xaml.cs
+++ Harjoitus17TaloWPF(kt)/Harjoitus17TaloWPF(kt)/MainWindow.xaml.cs
@@ -20,19 +20,59 @@ namespace Harjoitus17TaloWPF_kt_
     /// </summary>
     public partial class MainWindow : Window
     {
+        float lämpötila;
+        bool keittiöValot;
+        bool olohuoneenValot;
+        bool oviLukossa;
+
         public MainWindow()
         {
             InitializeComponent();
+            Textblocko4.Text = "0℃";
         }
+        public float Kasvatus;
+        public float Vähennys;
 
         private void KitchenlightOn(object sender, RoutedEventArgs e)
         {
-
+            TextBlock1.Text = "Keittiö Valo: On";
         }
 
         private void KitchenlightOff(object sender, RoutedEventArgs e)
         {
+            TextBlock1.Text = "Keittiö Valo: Off";
+        }
 
+        private void OloHuoneIsOn(object sender, RoutedEventArgs e)
+        {
+            TextBlock2.Text = "OloHuone Valo: On";
+        }
+
+        private void OloHuoneisOff(object sender, RoutedEventArgs e)
+        {
+            TextBlock2.Text = "OloHuone Valo: Off";
+        }
+
+        private void Ovi_Lukossa(object sender, RoutedEventArgs e)
+        {
+            TextBlock3.Text = "Ovi Lukossa";
+        }
+
+        private void Ovi_Auki(object sender, RoutedEventArgs e)
+        {
+            TextBlock3.Text = "Ovi Avattu";
+        }
+
+        private void Lisää_onClick(object sender, RoutedEventArgs e)
+        {// Lisää Lämpötilaa
+            Kasvatus +=1.0f;
+            Textblocko4.Text = Kasvatus.ToString();
+        }
+
+        private void Vähennys_OnClick(object sender, RoutedEventArgs e)
+        {// vähentää lämpötila
+            Vähennys -= 1.0f;
+            Textblocko4.Text = Vähennys.ToString();
         }
     }
 }
