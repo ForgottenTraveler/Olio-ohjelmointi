480ad536cbe51677955479d9b66e1138b1314ab0
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-10-12 12:52:00 +0300

    Coomit new hissi17

---
+++ Harjoitus19Hissi/Harjoitus19Hissi/Hissisivusto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus19Hissi
+{
+    public class Hissisivusto
+    {
+        private int kerros;
+
+        public int Kerros
+        {
+            get { return kerros; }
+            set
+            {
+                if (value >= 1 && value <= 6)
+                {
+                    kerros = value;
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen kerros! Käytä arvoa 1-6.");
+                }
+            }
+        }
+    }
+}
+   
---
+++ Harjoitus19Hissi/Harjoitus19Hissi/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus19Hissi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Hissisivusto hissi = new Hissisivusto();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Siirry_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtKerros.Text, out int syotettyKerros))
+            {
+                hissi.Kerros = syotettyKerros;
+                // Voit myös päivittää käyttöliittymää, esimerkiksi näyttämällä nykyisen kerroksen.
+                MessageBox.Show($"Olet kerroksessa {hissi.Kerros}");
+            }
+            else
+            {
+                MessageBox.Show("Virheellinen syöte! Anna kokonaisluku.");
+            }
+        }
+    }
+}
