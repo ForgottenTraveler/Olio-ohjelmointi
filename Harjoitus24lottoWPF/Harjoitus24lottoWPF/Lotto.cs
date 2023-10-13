e1dde36e4945fa96af301b2ee4abdac662d94837
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-10-13 11:51:24 +0300

    Sin coomiiit harjoitus24

---
+++ Harjoitus24lottoWPF/Harjoitus24lottoWPF/Lotto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus24lottoWPF
+{
+    internal class Lotto
+    {
+        private Random random = new Random();
+        public int[] ArvoRivi(int numeroidenMaara, int alaraja, int ylaraja)
+        {
+            return Enumerable.Range(alaraja, ylaraja - alaraja + 1)
+                .OrderBy(x => random.Next())
+                .Take(numeroidenMaara)
+                .ToArray();
+        }
+        
+        public string TulostaRivi(int[] rivi)
+        {//tulostaa rivin asiat
+            return string.Join(", ", rivi);
+        }
+    }
+}
---
+++ Harjoitus24lottoWPF/Harjoitus24lottoWPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus24lottoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Lotto lotto = new Lotto();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTulosta_Click(object sender, RoutedEventArgs e)
+        {
+            txtArvotutRivit.Text = ""; // Tyhjentää ensin
+
+            if (int.TryParse(txtMontaRivia.Text, out int montaRivia))
+            {
+                for (int i = 0; i < montaRivia; i++)
+                {
+                    int[] rivi = ArvoRivi();
+                    txtArvotutRivit.Text += $"{lotto.TulostaRivi(rivi)}\n";
+                }
+            }
+            else
+            {// näyttä messagebox josta kun käyttäjä ei laitanny sopiva numero riville.
+                MessageBox.Show("Syötä kelvollinen numero riveille.");
+            }
+        }
+        private int[] ArvoRivi()
+        {
+            int alaraja = 1;
+            int ylaraja = 1;
+            switch (cmbPelivalinta.SelectedIndex)
+            {
+                case 0: 
+                    ylaraja = 40;
+                    break;
+                case 1: 
+                    ylaraja = 48;
+                    break;
+                case 2: 
+                    ylaraja = 50;
+                    break;
+            }
+            return lotto.ArvoRivi(7, alaraja, ylaraja);
+        }
+        private void btnTyhjenna_Click(object sender, RoutedEventArgs e)
+        {
+            txtArvotutRivit.Text = "";
+        }
+        private void btnViikonArvonta_Click(object sender, RoutedEventArgs e)
+        {
+            txtArvotutRivit.Text = "Viikon Arvonta:\n";
+            int[] rivi = ArvoRivi();
+            txtArvotutRivit.Text += $"{lotto.TulostaRivi(rivi)}\n";
+            // Tarkista voitot ja muut lisätoiminnot voivat tulla tähän
+        }
+    }
+}
