e81df08bc76b3297681f861a588e20ced7a24065
ForgottenTraveler <Antti.Nguyen@student.careeria.fi>
2023-11-17 13:58:50 +0200

    Viimeinen tehtävä valmist harjoitus 26

---
+++ Harjoitus26Omasovellus/Harjoitus26Omasovellus/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus26Omasovellus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> tasks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<string>();
+            taskListBox.ItemsSource = tasks;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        { //Nappula joka lisää kun käyttäjä on syöttänyt
+            if (!string.IsNullOrWhiteSpace(taskTextBox.Text))
+            {
+                tasks.Add(taskTextBox.Text);
+                taskTextBox.Clear();
+            }
+        }
+
+        private void RemoveTask_Click(object sender, RoutedEventArgs e)
+        { //valittu asia joka on lisätty poistetaan
+            if (taskListBox.SelectedItem != null)
+            {
+                tasks.Remove(taskListBox.SelectedItem.ToString());
+            }
+        }
+    }
+}
